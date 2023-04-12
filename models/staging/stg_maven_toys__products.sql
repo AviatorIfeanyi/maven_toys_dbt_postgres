f0dfd428a2c166700f9030c565e94252c3917024
AviatorIfeanyi <inboxlegacy@gmail.com>
2023-04-12 17:42:42 +0100

    modify code

--- models/staging/stg_maven_toys__products.sql
+++ models/staging/stg_maven_toys__products.sql
@@ -2,5 +2,7 @@ WITH products AS (
   SELECT * FROM {{ source('maven_toys', 'products') }}
 )
 
-SELECT product_id, product_name, product_category, product_price, product_cost
+SELECT 
+  product_id, product_name, product_category, 
+  product_price, product_cost
 FROM products
